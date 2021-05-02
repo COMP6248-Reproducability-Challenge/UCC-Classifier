adf779c3add18701455dc8528f5c3b0e78d2eb1d
Sai Pandian <saipandian97@gmail.com>
2021-05-02 15:40:13 +0100

    Add print statements

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ from dataset import Dataset
 from model_MNIST import UCC_Model_MNIST
 
 # number of epochs:
-n_epochs = 1000000
+n_epochs = 10
 
 # the min/max number of possible classes
 min_classes = 1
@@ -31,18 +31,26 @@ model = UCC_Model_MNIST(image_size=28, num_images=32, num_classes=num_classes, l
 split_dataset = Dataset(num_instances=num_instances, num_samples_per_class=num_samples_per_class, digit_arr=subsets_elements_arr, ucc_start=min_classes, ucc_end=max_classes)
 
 # train
-for i in tqdm(range(n_epochs)):
+for i in range(n_epochs):
+    print("Epoch:", str(i+1)+"/"+str(n_epochs))
+    print("Getting Next Batch...")
     train_batch = split_dataset.next_batch_train()
 
+    print("Training on Batch...")
     [train_loss_weighted, train_ucc_loss, train_ae_loss, train_ucc_acc, train_ae_acc] = model.classifier_model.train_on_batch(train_batch[0], train_batch[1])
     # can write these losses to a file to generate plots later
 
+    
     # do validation on every 10th epoch
     if i % 10 == 0:
+        print()
+        print("Getting Validation Batch...")
         validation_batch = split_dataset.next_batch_val()
+        print("Testing on Batch...")
         [val_loss_weighted, val_ucc_loss, val_ae_loss, val_ucc_acc, val_ae_acc] = model.classifier_model.test_on_batch(validation_batch[0], validation_batch[1])
         # can write these losses to a file to generate plots later
 
+    print()
     print("Subset:                   ", subsets_elements_arr)
     print("Train Loss Weighted:      ", train_loss_weighted)
     print("Train UCC Accuracy:       ", train_ucc_acc)
@@ -52,6 +60,7 @@ for i in tqdm(range(n_epochs)):
     print("Validation UCC Accuracy:  ", val_ucc_acc)
     print("Validation UCC Loss:      ", val_ucc_loss)
     print("Validation AE Loss:       ", val_ae_loss)
+    print("................................................................")
 
 print("Training Finished!")
 model.classifier_model.save("./MNIST_classifier_weights.h5")
